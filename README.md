c4e3738824ec841abd241e990300b698df8fb18f
Kien Nguyen <kiennt2609@gmail.com>
2022-04-05 10:19:28 +0700

    Init commit

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # Polybar Mail
 
- A [Polybar](https://github.com/jaagr/polybar) module to show unread messages from mail.
+ A [Polybar](https://github.com/jaagr/polybar) module to show unread messages from mail inspired by [polybar-gmail](https://github.com/crabvk/polybar-gmail).
+
+## Dependencies
+
+ - **Font Awesome** - default email icon.
+- **canberra-gtk-play** - new email sound notification (optional).
+
+## Installation
+
+- Get polybarmail
+
+```bash
+git clone http://github.com/ntk148v/polybar-mail.git
+cp polybarmail.py ~/.config/polybar/polybarmail.py
+cp mail.ini ~/.config/polybar/mail.ini
+# Modify mail.ini with your mail configuration
+```
+
+- Scripts arguments
+
+```bash
+usage: polybarmail.py [-h] [--config CONFIG] [-p PREFIX] [-c COLOR] [-ns]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --config CONFIG
+  -p PREFIX, --prefix PREFIX
+  -c COLOR, --color COLOR
+  -ns, --nosound
+```
+
+- Update polybar config
+
+```ini
+[module/mail]
+type = custom/script
+exec = ~/.config/polybar/polybarmail.py
+tail = true
+click-left = xdg-open https://<your-mail-website>
+```
---
+++ polybarmail.py
@@ -0,0 +1,78 @@
+import argparse
+import configparser
+import imaplib
+from pathlib import Path
+import subprocess
+import sys
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', default='$HOME/.config/polybar/mail.ini')
+parser.add_argument('-p', '--prefix', default='\uf0e0')
+parser.add_argument('-c', '--color', default='#e06c75')
+parser.add_argument('-ns', '--nosound', action='store_true')
+args = parser.parse_args()
+
+unread_prefix = '%{F' + args.color + '}' + args.prefix + ' %{F-}'
+error_prefix = '%{F' + args.color + '}\uf06a %{F-}'
+count_was = 0
+
+# Parse config file
+config = configparser.ConfigParser()
+config.read(args.config)
+
+try:
+    mail_protocol = config['default']['mail_protocol']
+    mail_server = config['default']['mail_server']
+    mail_port = config['default']['mail_port']
+    mail_username = config['default']['mail_username']
+    mail_password = config['default']['mail_password']
+    mail_box = config['default']['mail_box']
+except KeyError as e:
+    print(error_prefix + f'wrong config: {str(e)}', flush=True)
+    sys.exit(1)
+
+
+def print_count(count, is_odd=False):
+    tilde = '~' if is_odd else ''
+    output = ''
+    if count > 0:
+        output = unread_prefix + tilde + str(count)
+    else:
+        output = (args.prefix + ' ' + tilde).strip()
+    print(output, flush=True)
+
+
+def update_count(count_was):
+    # Connect to mail server
+    typ = data = None
+    if mail_protocol.upper() == 'IMAP':
+        imap = imaplib.IMAP4_SSL(mail_server, mail_port)
+        imap.login(mail_username, mail_password)
+        imap.select(mailbox=mail_box)
+        typ, data = imap.search(None, '(UNSEEN)')
+    else:
+        raise Exception(f'Unsupported mail protocol: {mail_protocol}')
+    if typ != 'OK':
+        raise Exception(f'Search command return {typ}')
+    count = len(data[0].split())
+    print_count(count)
+    if not args.nosound and count_was < count and count > 0:
+        subprocess.run(['canberra-gtk-play', '-i', 'message'])
+    return count
+
+
+print_count(0, True)
+
+while True:
+    try:
+        if Path(args.config).is_file():
+            count_was = update_count(count_was)
+            time.sleep(10)
+        else:
+            print(error_prefix +
+                  f'config file {args.config} not found', flush=True)
+            time.sleep(2)
+    except Exception as e:
+        print(error_prefix + f'something went wrong: {str(e)}', flush=True)
+        sys.exit(1)
